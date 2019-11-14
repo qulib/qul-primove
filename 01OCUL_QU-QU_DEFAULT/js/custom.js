6be07e44bf38c198cfb35b3cdd7d52b6de1d3ba7
Paul Clifford <clifford@queensu.ca>
2019-11-14 14:53:32 -0500

    chat widget code, Omni logo

--- 01OCUL_QU-QU_DEFAULT/js/custom.js
+++ 01OCUL_QU-QU_DEFAULT/js/custom.js
@@ -1,6 +1,26 @@
-(function(){
-"use strict";
-'use strict';
+(function() {
+  "use strict";
+  "use strict";
 
-var app = angular.module('viewCustom', ['angularLoad']);
+  var app = angular.module("viewCustom", ["angularLoad"]);
+
+  var libraryh3lpScript = document.createElement("script");
+  libraryh3lpSCript.id = "localScript";
+  libraryh3lpScript.src = "https://ca.libraryh3lp.com/js/libraryh3lp.js?767";
+  // document.body.appendChild(s);
+  document.getElementsByTagName("head")[0].appendChild(libraryh3lpScript);
+
+  // app.component("prmLogoAfter", {
+  //   template: '<div id="needs-js"></div>'
+  // });
+
+  // var jQueryScript = document.createElement("script");
+  // jQueryScript.src = "https://code.jquery.com/jquery-3.4.1.min.js";
+  // document.getElementsByTagName("head")[0].appendChild(jQueryScript);
+
+  // jQueryScript.onload = function() {
+  //   $(document).ready(function() {
+  //     $("prm-logo").append("<img src=\"custom/01OCUL_QU-QU_DEFAULT/img/qul-omni-logo-white.svg\" alt=\"Omni logo\">");
+  //   });
+  // };
 })();
