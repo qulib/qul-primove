9b32eb4af9e2076069fe505f354a0a98f4ae6a7d
quladmin <quladmin@queensu.ca>
2019-11-15 13:47:03 -0500

    fix JS to handle chat popup

--- 01OCUL_QU-QU_DEFAULT/js/custom.js
+++ 01OCUL_QU-QU_DEFAULT/js/custom.js
@@ -4,23 +4,16 @@
 
   var app = angular.module("viewCustom", ["angularLoad"]);
 
-  var libraryh3lpScript = document.createElement("script");
-  libraryh3lpSCript.id = "localScript";
-  libraryh3lpScript.src = "https://ca.libraryh3lp.com/js/libraryh3lp.js?767";
-  document.body.appendChild(libraryh3lpSCript);
-  // document.getElementsByTagName("head")[0].appendChild(libraryh3lpScript);
-
-  // app.component("prmLogoAfter", {
-  //   template: '<div id="needs-js"></div>'
-  // });
-
-  // var jQueryScript = document.createElement("script");
-  // jQueryScript.src = "https://code.jquery.com/jquery-3.4.1.min.js";
-  // document.getElementsByTagName("head")[0].appendChild(jQueryScript);
-
-  // jQueryScript.onload = function() {
-  //   $(document).ready(function() {
-  //     $("prm-logo").append("<img src=\"custom/01OCUL_QU-QU_DEFAULT/img/qul-omni-logo-white.svg\" alt=\"Omni logo\">");
-  //   });
-  // };
 })();
+
+/*----------below is the code for libraryh3lp -----------*/
+var s=document.createElement('script');
+s.id='localScript';
+s.src= 'https://ca.libraryh3lp.com/js/libraryh3lp.js?776';
+document.body.appendChild(s);
+
+app.component('prmLogoAfter', {
+   template: '<div id="needs-js"></div>'
+});
+/*---------------libchat code ends here---------------*/
+
