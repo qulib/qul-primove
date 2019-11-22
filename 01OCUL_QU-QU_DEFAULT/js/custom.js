893726d1ac4c29c5f72ab927481d7610349d64e6
Paul Clifford <clifford@queensu.ca>
2019-11-22 10:51:35 -0500

    fixed chat loading text, trying JS branding things

--- 01OCUL_QU-QU_DEFAULT/js/custom.js
+++ 01OCUL_QU-QU_DEFAULT/js/custom.js
@@ -4,15 +4,16 @@
 
   var app = angular.module("viewCustom", ["angularLoad"]);
 
-/*----------below is the code for libraryh3lp -----------*/
-var s=document.createElement('script');
-s.id='localScript';
-s.src= 'https://ca.libraryh3lp.com/js/libraryh3lp.js?776';
-document.body.appendChild(s);
+  document.title = "Omni | Queen's University Library";
 
-app.component('prmLogoAfter', {
-   template: '<div id="needs-js"></div>'
-});
-/*---------------libchat code ends here---------------*/
+  /*----------below is the code for libraryh3lp -----------*/
+  var s = document.createElement("script");
+  s.id = "localScript";
+  s.src = "https://ca.libraryh3lp.com/js/libraryh3lp.js?776";
+  document.head.appendChild(s);
 
+  // app.component("prmLogoAfter", {
+  //   template: '<div id="needs-js"></div>'
+  // });
+  /*---------------libchat code ends here---------------*/
 })();
