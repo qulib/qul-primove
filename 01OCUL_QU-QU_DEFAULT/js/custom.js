b7d5f24117c3e0e83533169397c78ef1d56c69e4
Queens' University Library <quladmin@queensu.ca>
2020-06-04 08:45:15 -0400

    Resetting to ExL baseline package (commit bfed95c980139dd9f419b3b6b7dd857d1c15bb42 / Feb 5/2020) to include help files, documentation, etc.

--- 01OCUL_QU-QU_DEFAULT/js/custom.js
+++ 01OCUL_QU-QU_DEFAULT/js/custom.js
@@ -1,13 +1,19 @@
-(function() {
-  "use strict";
-  "use strict";
+(function () {
+    "use strict";
+    'use strict';
 
-  var app = angular.module("viewCustom", ["angularLoad"]);
 
-  /*----------below is the code for libraryh3lp -----------*/
-  var s = document.createElement("script");
-  s.id = "localScript";
-  s.src = "https://ca.libraryh3lp.com/js/libraryh3lp.js?776";
-  document.head.appendChild(s);
-  /*---------------libchat code ends here---------------*/
+    var app = angular.module('viewCustom', ['angularLoad']);
+
+    /****************************************************************************************************/
+
+        /*In case of CENTRAL_PACKAGE - comment out the below line to replace the other module definition*/
+
+        /*var app = angular.module('centralCustom', ['angularLoad']);*/
+
+    /****************************************************************************************************/
+
+
 })();
+
+   
