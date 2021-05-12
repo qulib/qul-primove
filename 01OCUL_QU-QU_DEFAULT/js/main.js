9d7c4a3f932d16846ef22b48cff5550e2b2d9c85
sulfurchesh <r.alex.fletcher@gmail.com>
2021-05-12 10:28:43 -0400

    Updating with HT changes
    Note: We can no longer "zip" this up to build.

--- 01OCUL_QU-QU_DEFAULT/js/main.js
+++ 01OCUL_QU-QU_DEFAULT/js/main.js
@@ -1,6 +1,21 @@
+import 'primo-explore-hathitrust-availability';
 (function () {
-  "use strict";
-  "use strict";
+    'use strict';
+    var app = angular.module('viewCustom', ['angularLoad', 'hathiTrustAvailability']);
+    // Include HathiTrust full text links.
+    app.component('prmSearchResultAvailabilityLineAfter', {
+        template: '<hathi-trust-availability msg="Full Text Available Online at HathiTrust" ignore-copyright="true" entity-id="https://login.queensu.ca/idp/shibboleth"></hathi-trust-availability>'
+    })
+/*----------below is the code for libraryh3lp -----------*/
+var x = document.createElement("script");
+x.type = "text/javascript";
+x.async = true;
+x.src = "https://ca.libraryh3lp.com/js/libraryh3lp.js?776";
+var y = document.getElementsByTagName("script")[0];
+y.parentNode.insertBefore(x, y);
 
-  var app = angular.module("viewCustom", ["angularLoad"]);
+var chat = document.createElement("div");
+chat.innerHTML = '<div class="needs-js"></div>';
+document.body.appendChild(chat);
+/*--------------- libraryh3lp code ends here---------------*/
 })();
